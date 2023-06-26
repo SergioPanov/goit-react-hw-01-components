c9a989a8ac6e7320346f806d8c380c1b88f3b274
SergioPanov <www.vapanov@gmail.com>
2023-06-27 00:25:15 +0200

    hw-fixed-again

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,27 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export const Statistics = ({ title, stats }) => {
+export const Statistics = props => {
+  const { title, stats } = props;
+
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className={css.statList}>
-        {stats.map(({ id, label, percentage }) => (
-          <li
-            key={id}
-            style={{ backgroundColor: randomColor() }}
-            className={css.statItem}
-          >
-            <span className={css.label}>{label} </span>
-            <span className={css.percentage}>{percentage}%</span>
-          </li>
-        ))}
-      </ul>
+      {stats.length > 0 && (
+        <ul className={css.statList}>
+          {stats.map(({ id, label, percentage }) => (
+            <li
+              key={id}
+              style={{ backgroundColor: randomColor() }}
+              className={css.statItem}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
@@ -34,5 +38,11 @@ const randomColor = () => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.array.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
